4a520f85bc7c82a41aeebeab9f771a8903056ef2
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-08 13:59:30 +0700

    feat: remove main storyboard

--- CoreDataTodos/Screens/Home/ViewModel/HomeViewModel.swift
+++ CoreDataTodos/Screens/Home/ViewModel/HomeViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by 7Peaks on 8/8/2565 BE.
 //
 
-import Foundation
+import CoreData
+import UIKit
+
+class HomeViewModel {
+  // MARK: - Properties
+
+  // MARK: - Init
+
+  // MARK: - Methods
+  
+  func getTodos() {}
+  
+  func createTodo(with name: String) {}
+  
+  func deleteTodo(todo: Todo) {}
+  
+  func updateTodo(todo: Todo) {}
+}
