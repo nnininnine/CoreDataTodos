c2ed3f730fa54159994c269f051a2a9fbecdeb38
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-09 09:51:23 +0700

    feat(home): implement getTodos func

--- CoreDataTodos/Screens/Home/ViewModel/HomeViewModel.swift
+++ CoreDataTodos/Screens/Home/ViewModel/HomeViewModel.swift
@@ -11,15 +11,25 @@ import UIKit
 class HomeViewModel {
   // MARK: - Properties
 
+  private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  var todos: [Todo] = []
+
   // MARK: - Init
 
   // MARK: - Methods
-  
-  func getTodos() {}
-  
+
+  func getTodos() {
+    do {
+      todos = try context.fetch(Todo.fetchRequest())
+    } catch let e {
+      // error
+      print(e)
+    }
+  }
+
   func createTodo(with name: String) {}
-  
+
   func deleteTodo(todo: Todo) {}
-  
+
   func updateTodo(todo: Todo) {}
 }
