6aa9a7b77ec39c169cbf7073153795293c1b9f98
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-09 12:13:36 +0700

    feat(home): add preview

--- CoreDataTodos/Screens/Home/View/HomeViewController.swift
+++ CoreDataTodos/Screens/Home/View/HomeViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  // MARK: - Properties
+
+  // MARK: - LifeCycles
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = .systemBackground
     title = "Core Data Todos"
   }
+
+  // MARK: - Methods
 }
+
+// MARK: - Previews
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct HomeViewRepresentable: UIViewRepresentable {
+  func makeUIView(context: Context) -> some UIView {
+    return HomeViewController().view
+  }
+
+  func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
+struct HomeViewController_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeViewRepresentable()
+  }
+}
+#endif
