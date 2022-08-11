2c72006e27e9650a49a8d19a50fe8540d6da2258
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-11 11:04:39 +0700

    feat(todos): init view controller with folder

--- CoreDataTodos/Screens/Todos/View/TodosViewController.swift
+++ CoreDataTodos/Screens/Todos/View/TodosViewController.swift
@@ -8,19 +8,32 @@
 import UIKit
 
 class TodosViewController: UIViewController {
+  // MARK: - Properties
+
+  let folder: Folder
+
+  // MARK: - Subviews
+
+  // MARK: - Init
+
+  init(with folder: Folder) {
+    self.folder = folder
+
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - LifeCycles
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
   }
 
-  /*
-   // MARK: - Navigation
-
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       // Get the new view controller using segue.destination.
-       // Pass the selected object to the new view controller.
-   }
-   */
+  // MARK: - Methods
 }
