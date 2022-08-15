d447af5b68f9d18d85b8663b3b5a95d239b1b767
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-15 14:45:23 +0700

    feat(unit-test): create TestCoreDataStack class for unit testing

---
+++ CoreDataTodosTests/TestCoreDataStack.swift
@@ -0,0 +1,32 @@
+//
+//  TestCoreDataStack.swift
+//  CoreDataTodosTests
+//
+//  Created by 7Peaks on 15/8/2565 BE.
+//
+
+import CoreData
+import Foundation
+
+class TestCoreDataStack {
+  // MARK: - Properties
+
+  static let shared: TestCoreDataStack = .init()
+
+  lazy var persistentContainer: NSPersistentContainer = {
+    let description: NSPersistentStoreDescription = .init(url: URL(fileURLWithPath: "/dev/null"))
+    let container: NSPersistentContainer = .init(name: "CoreDataTodos")
+    container.persistentStoreDescriptions = [description]
+    container.loadPersistentStores(completionHandler: { _, error in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+
+    return container
+  }()
+
+  // MARK: - Init
+
+  private init() {}
+}
